9d62e4e35714274363d6d96de4e7aef524f72dbe
Suraj Yadav <sarry7045@gmail.com>
2025-02-25 14:19:49 +0530

    Second Commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16kb" })); // We can change it, according to our Requirments
+// app.use(express.json({ limit: "16kb" })); // We can change it, according to our Requirments
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // for Sapce in URL like - Best Deal - it's Covert to like this Best%Deal
 app.use(express.static("public"));
 app.use(cookieParser()); // for Access and Set the Cookies of Broswer
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+// import connectDB from "./db/index.js";
 import { app } from "./app.js";
+import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is Running on ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("DB Connection Failed", err);
-  });
+app.get("/api/getDetails/:city", async (req, res) => {
+  try {
+    const { city } = req.params;
+    const response = await axios.get(
+      `${process.env.WEATHER_API_URL}?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching weather data" });
+  }
+});
+
+app.get("/api/getDetails/:city/History", async (req, res) => {
+  try {
+    const { city } = req.params;
+    const response = await axios.get(`${process.env.WIKI_API_URL}${city}`);
+    res.json({ title: response.data.title, extract: response.data.extract });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching historical data" });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(`Server is Running on ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("DB Connection Failed", err);
+//   });
